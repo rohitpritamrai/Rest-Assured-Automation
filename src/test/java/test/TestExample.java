60eb7899fd32a14dccdaf95fa354942481a9001d
Shrinwanti22 <shrinwanti92@gmail.com>
2021-07-21 23:37:16 +0530

    new rest assured automation project

---
+++ src/test/java/test/TestExample.java
@@ -0,0 +1,20 @@
+package test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestExample {
+
+    @Test
+    public void firstTest(){
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getTime());
+        System.out.println(response.getBody().asString());
+        System.out.println(response.getStatusLine());
+        Assert.assertEquals(response.statusCode(),200);
+        System.out.println(response.getContentType());
+    }
+}
